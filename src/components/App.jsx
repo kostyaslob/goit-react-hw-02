995f11eaa4c696d6d9b158eec48b31248c68fa36
kostyaslob <kostyaslob@gmail.com>
2025-04-24 17:16:20 +0300

    add components and update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,17 @@
 import './App.css';
 
-import Profile from "./Profile/Profile";
-import FriendList from "./FriendList/FriendList";
-import TransactionHistory from "./TransactionHistory/TransactionHistory";
-
-import userData from "../userData.json";
-import friends from "../friends.json";
-import transactions from "../transactions.json";
-
+import Description from "./Description/Description";
+import Options from "./Options/Options";
+import Feedback from "./Feedback/Feedback";
+import Notification from "./Notification/Notification";
 
 const App = () => {
   return (
     <>
-      <Profile
-        name={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        image={userData.avatar}
-        stats={userData.stats}
-      />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
+      <Description />
+      <Options />
+      <Feedback />
+      <Notification />
     </>
   );
 };
