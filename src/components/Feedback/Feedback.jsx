3b92ed9d1f6daa08737894f7764f524fdc7af172
kostyaslob <kostyaslob@gmail.com>
2025-04-25 17:09:17 +0300

    add css styles to the components

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,6 +1,8 @@
+import css from "./Feedback.module.css"
+
 export default function Feedback({ values, totalFeedback, positiveFeedback }) {
     return (
-        <div>
+        <div className={css.feedback}>
           <p>Good: {values.good}</p>
           <p>Neutral: {values.neutral}</p>
           <p>Bad: {values.bad}</p>
--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,15 +1,17 @@
+import css from "./Options.module.css"
+
 export default function Options({ setValues, updateFeedback, totalFeedback }) {
     const resetFeedback = () => {
         setValues({ good: 0, neutral: 0, bad: 0 })
     }
     
     return (
-        <div>
+        <div className={css.option}>
             <button onClick={() => updateFeedback("good")}>Good</button>
             <button onClick={() => updateFeedback("neutral")}>Neutral</button>
             <button onClick={() => updateFeedback("bad")}>Bad</button>
             {totalFeedback > 0 &&
-                <button onClick={resetFeedback}>Reset</button>
+                <button onClick={resetFeedback} style={{backgroundColor: '#b1241a'}}>Reset</button>
             }
         </div>
     )
